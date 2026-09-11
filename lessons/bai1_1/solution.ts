b2f32d82c93debf61889aa66687b96a8ce5385b3
DaiLe-05 <ledai2k5mc@gmail.com>
2026-09-11 09:17:02 +0700

    Complete lesson bai1_1

--- lessons/bai1_1/solution.ts
+++ lessons/bai1_1/solution.ts
@@ -8,6 +8,19 @@ export type Block = {
 };
 
 // ✍️ TODO: Viết hàm tại đây
+import crypto from "crypto";
+
 export function isValidBlock(block: Block): boolean {
-  return false; // Chỉnh lại logic
+    const value =
+        block.index +
+        block.timestamp +
+        JSON.stringify(block.transactions) +
+        block.previous_hash;
+
+    const hash = crypto
+        .createHash("sha256")
+        .update(value)
+        .digest("hex");
+
+    return hash === block.current_hash;
 }
